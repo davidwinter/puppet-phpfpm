147b91ed3a3725e675daaa6a0ae03da67fb3c3cc
David Winter <i@djw.me>
2013-03-28 11:30:37 +0000

    puppet-lint passing.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,21 @@
+# == Class: phpfpm
+#
+# See README.md
+#
 class phpfpm (
-		$ensure = 'present',
-	) {
+    $ensure = 'present',
+) {
+  $running = $ensure ? {
+    absent  => 'stopped',
+    default => 'running',
+  }
 
-	$running = $ensure ? {
-		absent => 'stopped',
-		default => 'running',
-	}
-	
-	package { 'php5-fpm':
-		ensure => $ensure,
-	}
+  package { 'php5-fpm':
+    ensure => $ensure,
+  }
 
-	service { 'php5-fpm':
-		ensure => $running,
-		require => Package['nginx'],
-	}
-	
+  service { 'php5-fpm':
+    ensure  => $running,
+    require => Package['nginx'],
+  }
 }
